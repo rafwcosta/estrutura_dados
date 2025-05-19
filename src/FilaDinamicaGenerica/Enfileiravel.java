884bbfb3d19d923df21c93f902669f90b4c17562
Rafael Costa Oliveira <zrafaelcostaz@gmail.com>
2025-05-19 19:35:53 -0300

    Enfileiravel fila dinamica

---
+++ src/FilaDinamicaGenerica/Enfileiravel.java
@@ -0,0 +1,20 @@
+package FilaDinamicaGenerica;
+
+public interface Enfileiravel<T> {
+	void enfileirarInicio(T dado);
+	void enfileirarFim(T dado);
+	
+	T frente();
+	T tras();
+	
+	void atualizarInicio(T dado);
+	void atualizarFim(T dado);
+
+	T desenfileirarInicio();
+	T desenfileirarFim();
+
+	boolean estaCheia();	
+	boolean estaVazia();
+	String imprimirDeTrasPraFrente();
+	String imprimirDeFrentePraTras();	
+}
